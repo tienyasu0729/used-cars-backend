1ae8dc97e7eb0005df752bab0632986e62965b94
tienyasu0729 <tienngqueson@gmail.com>
2026-03-23 16:46:30 +0700

    create project

---
+++ src/main/java/scu/dn/used_cars_backend/UsedCarsBackendApplication.java
@@ -0,0 +1,13 @@
+package scu.dn.used_cars_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedCarsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedCarsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scu/dn/used_cars_backend/UsedCarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package scu.dn.used_cars_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedCarsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
